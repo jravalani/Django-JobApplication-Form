56242b224e63713f65eb8936f43208bb80bd87e1
Jay Ravalani <jay.ravalani746@gmail.com>
2024-08-26 21:58:45 +0530

    implement admin page

--- job_application/admin.py
+++ job_application/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Form2
 
 # Register your models here.
+
+
+class FormAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name", "email")
+    search_fields = ("first_name", "last_name", "email")
+    list_filter = ("date", "occupation")
+    ordering = ("first_name", )
+    readonly_fields = ("occupation", )
+
+
+admin.site.register(Form2, FormAdmin)
